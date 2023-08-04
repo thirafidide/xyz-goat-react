7f8ada56e18a1359dd7a30c6ec36bb88204e543f
Thirafi Dide <jurnaldide@gmail.com>
2023-08-04 16:27:48 +0700

    Simple component to add task

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -17,9 +17,44 @@ export default function TodoList() {
     <div className="todolist">
       <h1>TODO LIST</h1>
 
+      <AddTodoForm />
+
       {todos.map(({ id, title }) => (
         <li key={id}>{title}</li>
       ))}
     </div>
+  )
+}
+
+function AddTodoForm() {
+  const [newTodo, setNewTodo] = useState('')
+
+  /**
+   * @param {React.FormEvent<HTMLInputElement>} event
+   */
+  function handleChangeInput(event) {
+    setNewTodo(event.target.value)
+  }
+
+  /**
+   * @param {React.FormEvent<HTMLFormElement>} event
+   */
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log(newTodo)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="add-todo">New Tasks</label>
+      <input
+        type="text"
+        name="add-todo"
+        value={newTodo}
+        onChange={handleChangeInput}
+      />
+
+      <button type="submit">Add</button>
+    </form>
   )
 }
