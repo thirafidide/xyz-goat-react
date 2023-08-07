a98d50dc1d6b1d5da7e6b053920fdfb9cc149a3c
Thirafi Dide <jurnaldide@gmail.com>
2023-08-07 13:05:09 +0700

    Test error on useTodoList

--- src/hooks/__tests__/useTodoList.test.js
+++ src/hooks/__tests__/useTodoList.test.js
@@ -1,10 +1,19 @@
 import { renderHook } from '@testing-library/react-hooks/dom'
 import useTodoList from '../useTodoList'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { MOCK_INITIAL_TODO_LIST } from '../../mocks/server'
+import { MOCK_INITIAL_TODO_LIST, server } from '../../mocks/server'
+import { rest } from 'msw'
 
 function createTestWrapperComponent() {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        // Disable react query retry behaviour so we can test fail case faster
+        retry: false,
+      },
+    },
+  })
+
   const wrapper = ({ children }) => (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   )
@@ -32,4 +41,27 @@ test('should return todo list after finished fetching', async () => {
 
   expect(result.current.isLoading).toBe(false)
   expect(result.current.todoList).toMatchObject(MOCK_INITIAL_TODO_LIST)
+})
+
+test('should return error when failed to fetch', async () => {
+  // Mock network error like offline/connection failed
+  server.use(
+    rest.all('http://localhost:3001/tasks', (req, res) => {
+      return res.networkError('Network error')
+    }),
+  )
+
+  // Supress expected network error message
+  jest.spyOn(console, 'error').mockImplementation(() => {})
+
+  const wrapper = createTestWrapperComponent()
+  const { result, waitForNextUpdate } = renderHook(() => useTodoList(), {
+    wrapper,
+  })
+
+  await waitForNextUpdate()
+
+  expect(result.current.isLoading).toBe(false)
+  expect(result.current.isError).toBe(true)
+  expect(result.current.error).toBeInstanceOf(Error)
 })
